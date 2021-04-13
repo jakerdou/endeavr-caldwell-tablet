ebc5b9fad36a27e847e7d5ac270318a7ab89194e
Matthew Chen <mattokc35@tamu.edu>
2021-04-12 22:52:05 -0500

    changed database password to caldwell

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "howdy123",
+    password: "caldwell",
     database: "caldwell_water_collection",
     host: "localhost",
     port: 5432
